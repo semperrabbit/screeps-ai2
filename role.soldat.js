7179fa374bea7de353d920bfaa529e3ab90f88b1
semperrabbit <semperrabbit05@gmail.com>
2016-07-22 22:30:40 -0600

    initial upload

---
+++ role.soldat.js
@@ -0,0 +1,19 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.soldat');
+ * mod.thing == 'a thing'; // true
+ */
+
+module.exports = {
+    run: function(creep) {
+var targ = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+            // try to harvest energy, if the source is not in range
+            if (creep.attack(targ) == ERR_NOT_IN_RANGE) {
+                // move towards the source
+                creep.moveTo(targ);
+            }
+    }
+};
